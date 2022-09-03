54f988a6efa4f966cae2e7d70ef443d50ade81ec
George Sivtsev <gumben7@gmail.com>
2022-09-03 10:05:47 +0700

    problem 1 and problem 2

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 
-namespace Ci2207sg1AlgBaseLab1
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace Ci2207sg1AlgBaseLab1 { 
+    class Program {
+        static void Main(string[] args) {
+            int a = 3;
+            int b = 7;
+            a = a ^ b;
+            b = a ^ b;
+            a = a ^ b;
+            Console.WriteLine(a + " " + b);
+            double c = Math.Pow(Math.Max(Math.Round(Math.Abs(-Math.Sqrt(3457))), 48), 1);
+            Console.WriteLine(c);
         }
     }
 }
