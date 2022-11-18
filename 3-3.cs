04e88506a49a1bbd6f3ddfe5aafb313ee7786002
Егор Сивцев <gumben7@gmail.com>
2022-11-19 04:15:26 +0700

    broken 3-3

--- 3-3.cs
+++ 3-3.cs
@@ -9,6 +9,7 @@ class Problem3_3 {
         var n = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
         StringBuilder str = new StringBuilder("0.");
         int w;
+        var cf = false;
         for (; k > 0; --k) {
             n *= b;
             w = (int)n;
@@ -21,23 +22,53 @@ class Problem3_3 {
             n -= w;
             if (n == 0) {
                 break;
-            }
-            var cf = false;
-            if (k == 0) {
+            } //15 6 0,123456 0,1cb9e6
+            //3 6 0.234 0.200220
+            if (k == 1) {
                 do {
                     n *= b;
                     w = (int)n;
                     if (w < (double)b/2 - 1) {
                         break;
                     }
-                    if (w >= )
+                    if (w >= (double)b/2) {
+                        cf = true;
+                        break;
+                    }
                 } while (true);
-            }
+            }//*/
+        }
+        if (cf) {
+            int a;
+            char c;
+            var m = 1;
+            do {
+                c = str[str.Length-m];
+                if (c <= '9') {
+                    a = c - '0';
+                }
+                else {
+                    a = c - 'a' + 10;
+                }
+                ++a;
+                if (a < b) {
+                    if (a < 10) {
+                        str[str.Length-m] = (char)(a + '0');
+                    }
+                    else {
+                        str[str.Length-m] = (char)(a + 'a' - 10);
+                    }
+                    break;
+                }
+                else {
+
+                }
+                ++m;
+            } while (true);
         }
         while (str[str.Length-1] == '0') {
             --str.Length;
         }
         Console.WriteLine(str);
-        var t = 3;
     }
 }
